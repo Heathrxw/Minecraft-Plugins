44effd19183d51d5d6b9be11883c3de36103e44d
Heathrxw <Heathrxw@gmail.com>
2020-11-06 00:21:47 +0000

    Added "MultiplyDrops" Plugin

---
+++ MultiplyDrops/src/Heathrxw/Xenon/MultiplyDrops/MultiplyDrops.java
@@ -0,0 +1,15 @@
+package Heathrxw.Xenon.MultiplyDrops;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MultiplyDrops extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        CommandMultiply commandMultiply = new CommandMultiply();
+
+        this.getCommand("drops").setExecutor(commandMultiply);
+        getServer().getPluginManager().registerEvents(commandMultiply, this);
+
+    }
+}
