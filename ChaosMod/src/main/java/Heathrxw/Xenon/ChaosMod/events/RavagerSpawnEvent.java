1f43f4dee045a66371d055e130fe75c6259f6f01
Heathrxw <Heathrxw@gmail.com>
2020-11-23 23:32:57 +0000

    Added Ravager Event

---
+++ ChaosMod/src/main/java/Heathrxw/Xenon/ChaosMod/events/RavagerSpawnEvent.java
@@ -0,0 +1,27 @@
+package Heathrxw.Xenon.ChaosMod.events;
+
+import Heathrxw.Xenon.ChaosMod.Chaos;
+import Heathrxw.Xenon.ChaosMod.ChaosEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+
+public class RavagerSpawnEvent extends ChaosEvent {
+    public void onEnable(String username, Chaos plugin) {
+        Player target = (Bukkit.getServer().getPlayer(username));
+
+        if(target != null) {
+            target.getWorld().spawnEntity(target.getLocation().add(0, 3, 0), EntityType.RAVAGER);
+            target.getWorld().spawnEntity(target.getLocation().add(0, 3, 0), EntityType.RAVAGER);
+            target.getWorld().spawnEntity(target.getLocation().add(0, 3, 0), EntityType.RAVAGER);
+            target.getWorld().spawnEntity(target.getLocation().add(0, 3, 0), EntityType.RAVAGER);
+            target.getWorld().spawnEntity(target.getLocation().add(0, 3, 0), EntityType.RAVAGER);
+            target.getWorld().spawnEntity(target.getLocation().add(0, 3, 0), EntityType.RAVAGER);
+
+        }
+    }
+
+    public String getName() {
+        return "Ravagers";
+    }
+}
